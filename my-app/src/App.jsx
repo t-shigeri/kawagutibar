cca01fb93806d91d737dbd45d8c275aa88e1ad76
t-shigeri <fko2447037@stu.o-hara.ac.jp>
2025-09-08 15:12:44 +0900

    修正

--- my-app/src/App.jsx
+++ my-app/src/App.jsx
@@ -2,9 +2,7 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-@tailwind base;
-@tailwind components;
-@tailwind utilities;
+
 function App() {
   const [count, setCount] = useState(0)
 
